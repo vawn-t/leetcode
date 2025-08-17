72271ee85603daaecc4c871e315fbce509cfe2b0
vawn <vantran99dn@gmail.com>
2025-08-17 21:43:12 +0700

    feat: implement longestRepeatedSubarray function to find the length of the longest repeated subarray

--- longest-repearted-subarray.js
+++ longest-repearted-subarray.js
@@ -1,4 +1,4 @@
-const longestRepeatedSubarray = (nums) => {
+var longestRepeatedSubarray = function (nums) {
   if (nums.length === 0) return 0;
 
   let maxLength = 1;
@@ -14,6 +14,22 @@ const longestRepeatedSubarray = (nums) => {
   }
 
   return maxLength;
+};
+
+var longestRepeatedSubarray = function (nums) {
+  if (nums.length === 0) return 0;
+
+  let left = 0;
+  let max = 1;
+
+  for (let right = 1; right < nums.length; right++) {
+    if (nums[left] !== nums[right]) {
+      left = right;
+    }
+    max = Math.max(max, right - left + 1);
+  }
+
+  return max;
 };
 
 console.log(longestRepeatedSubarray([0, 0, 1, 1, 1, 1, 2, 2])); // 4
